41e6fc8839cc866501fa4a4cb54c09aaddd0489c
Mauricio.Weyll <msweyll@gmail.com>
2023-11-23 23:52:41 -0300

    Conclusão do exercício de tabela Aula14 + intro JS

---
+++ AULA17/exemplo1/app.js
@@ -0,0 +1,111 @@
+let frase = " Alo alunos da turma 7 Arnia"
+console.log(frase)
+
+let numero = 1
+console.log(numero)
+
+let numeroDecimal = 1.5
+console.log(numeroDecimal)
+
+let soma = 2 + 2
+console.log(soma)
+
+let subtracao = 4 - 2
+console.log(subtracao)
+
+//Exercício Aula 01 JS
+
+const nome = "Mauricio Weyll Souza"
+console.log (nome)
+
+const dataNascimento = "18.02.1988"
+console.log (dataNascimento)
+
+const cpf = "xxx.xxx.xxx-xx"
+console.log (cpf)
+
+const rg = "504165-9"
+console.log (rg)
+
+const email = "msweyll@gmail.com"
+console.log (email)
+
+// Escrever a letra de uma música
+
+let musica = `Amor Puro
+O que há dentro do meu coração
+Eu tenho guardado pra te dar
+E todas as horas que o tempo
+Tem pra me conceder
+São tuas até morrer
+E a tua história, eu não sei
+Mas me diga só o que for bom
+Um amor tão puro que ainda nem sabe
+A força que tem
+É teu e de mais ninguém
+Te adoro em tudo, tudo, tudo
+Quero mais que tudo, tudo, tudo
+Te amar sem limites
+Viver uma grande história
+Te adoro em tudo, tudo, tudo
+Quero mais que tudo, tudo, tudo
+Te amar sem limites
+Viver uma grande história
+Aqui ou noutro lugar
+Que pode ser feio ou bonito
+Se nós estivermos juntos
+Haverá um céu azul
+Um amor puro
+Não sabe a força que tem
+Meu amor, eu juro
+Ser teu e de mais ninguém
+Um amor puro
+O que há dentro do meu coração
+Eu tenho guardado pra te dar
+E todas as horas que o tempo
+Tem pra me conceder
+São tuas até morrer
+E a tua história, eu não sei
+Mas me diga só o que for bom
+Um amor tão puro que ainda nem sabe
+A força que tem
+É teu e de mais ninguém
+Te adoro em tudo, tudo, tudo
+Quero mais que tudo, tudo, tudo
+Te amar sem limites
+Viver uma grande história
+Te adoro em tudo, tudo, tudo
+Quero mais que tudo, tudo, tudo
+Te amar sem limites
+Viver uma grande história
+Te adoro em tudo
+Quero mais que tudo
+Te amar sem limites
+Viver uma grande história
+Te amar sem limites
+Viver uma grande história
+Te adoro em tudo, tudo, tud
+Quero mais que tudo, tudo, tudo
+Te amar sem limites
+Viver uma grande história`
+console.log (musica)
+
+//Exercício 3
+
+let operacao = x = 2 + 3 * 4
+console.log(operacao)
+
+//Exercício 4
+
+let expressao = 10 - "2"
+console.log(expressao)
+
+//Exercício 5
+
+let raizCubica = Math.sqrt(13)
+console.log(raizCubica)
+
+//Exercício 6
+
+let expressao1 = 2 * 1.01 ** 12
+console.log(expressao1)
