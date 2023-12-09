8131d82bf164471822d64d4604d061796d7700de
Mauricio.Weyll <msweyll@gmail.com>
2023-12-08 23:37:38 -0300

    aula 25

--- AULA25/ex3.js
+++ AULA25/ex3.js
@@ -1,1 +1,5 @@
 const io = require("../Io/io")
+
+/* Faça um programa de computador para contar e imprimir a
+quantidade de divisores que um número natural possui. Este número
+deverá ser fornecido pelo usuário. */
